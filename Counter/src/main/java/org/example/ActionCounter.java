5f751ab4eba70c9008ed9078af43b62e886d6c2d
Aidar <myakkaynt228@gmail.com>
2024-01-19 09:48:17 +0300

    perf increased

--- Counter/src/main/java/org/example/ActionCounter.java
+++ Counter/src/main/java/org/example/ActionCounter.java
@@ -1,7 +1,7 @@
 package org.example;
 
-import java.util.List;
 import java.util.LinkedList;
+import java.util.List;
 
 public final class ActionCounter {
     private static final int TIME_THRESHOLD = 300;
@@ -17,11 +17,18 @@ public final class ActionCounter {
 
     public int getActions(int timestamp) {
         int count = 0;
-        for (int action : actions) {
+
+        for (int i = actions.size() - 1; i >= 0; i--) {
+            int action = actions.get(i);
+
             if (timestamp - action >= 0 && timestamp - action < TIME_THRESHOLD) {
                 count++;
+            } else if (timestamp - action >= TIME_THRESHOLD) {
+                break;
             }
         }
+
         return count;
     }
+
 }
